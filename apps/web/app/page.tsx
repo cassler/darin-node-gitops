05fec4d77a27a13b7b862b5702e53d5c73462328
Darin Cassler <darin@cassler.net>
2023-05-27 15:38:19 -0500

    feat: sample banners

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,24 @@
 import { Button, Header } from "ui";
+import fixtures from "../fixtures/copywriting.json";
 
+function getWordFrom(text: string, index: number) {
+  return text.split(" ").slice(0, index).join(" ");
+}
 export default function Page() {
   return (
     <>
-      <Header text="darin.co" />
-      <div className="container mx-auto py-4 gap-1">
-        <div className="prose">
-          <p>Welcome to the party</p>
+      <Header text="launchcg.com" />
+      <div className="container mx-auto p-4 gap-1">
+        <div className="space-y-4 max-w-[40rem]">
+          <p className="text-xs drop-shadow drop-shadow-white uppercase text-slate-500 font-medium">
+            {getWordFrom(fixtures.kicker, 3)}
+          </p>
+          <h1 className="text-4xl font-medium text-slate-900 tracking-tight">
+            {fixtures.headline}
+          </h1>
+          <p className="text-slate-800">{fixtures.lorem}</p>
+          <p className="text-sm text-slate-500">{fixtures.lorem}</p>
+          <p className="text-xs text-slate-400">{fixtures.lorem}</p>
           <Button />
         </div>
       </div>
