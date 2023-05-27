a1bd921aac7386744b420ec6a311bf09c14c7ea1
Darin Cassler <darin@cassler.net>
2023-05-27 15:42:38 -0500

    chore: initialze husky

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -2,6 +2,14 @@
 
 import * as React from "react";
 
-export const Button = (props:React.HTMLAttributes<HTMLButtonElement>) => {
-  return <button {...props} className='p-1 px-2 border border-slate-200 text-slate-600 font-medium tracking-tight rounded' onClick={() => alert("boop")}>{props.children || 'Boop'}</button>;
+export const Button = (props: React.HTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      {...props}
+      className="p-1 px-2 border border-slate-200 text-slate-600 font-medium tracking-tight rounded"
+      onClick={() => alert("boop")}
+    >
+      {props.children || "Boop"}
+    </button>
+  );
 };
