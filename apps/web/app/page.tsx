4c69a2f6dc6aaea0758c07dd12938d8afb522229
Darin Cassler <darin@cassler.net>
2023-05-27 13:07:37 -0500

    basic github actions init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,8 +4,9 @@ export default function Page() {
   return (
     <>
       <Header text="darin.co" />
-      <div className="container mx-auto py-4">
+      <div className="container mx-auto py-4 gap-1 flex">
         <Button />
+        <Button>Hello</Button>
       </div>
     </>
   );
--- packages/ui/Header.tsx
+++ packages/ui/Header.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 
+import { GiftIcon, GlobeAmericasIcon } from '@heroicons/react/24/outline';
 export const Header = ({ text }: { text: string }) => {
   return (
     <div className="h-20 bg-white flex items-center shadow-sm">
       <div className="container mx-auto">
-        <h1 className='text-2xl tracking-tight font-semibold'>{text}</h1>
+
+        <div className="flex gap-2 items-center">
+          <GlobeAmericasIcon className='w-8 text-blue-500' />
+          <h1 className='text-2xl tracking-tight font-semibold'>{text}</h1>
+        </div>
       </div>
     </div>
   )
